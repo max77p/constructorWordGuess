d941116bacab5c6aad86c3b215af9c93f6604d77
max77p <shanindrakumar@gmail.com>
2018-06-27 21:41:21 -0400

    working game

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inquirer = require('inquirer');
 var word = require("./word.js");
 
+
 var words = {
     City: ["toronto", "california", "london", "montreal"],
     Sports: ["soccer", "curling", "jordan", "stadium", "rink"],
@@ -20,31 +21,88 @@ var pickcategory = function () {
     var temphangmanarr = temprandomword.toLowerCase().split("")
     return temphangmanarr;
 }
-var wordToUse=pickcategory().join("");
+var wordToUse = pickcategory().join("");
 // console.log(x);
 
 var word1;
-var startGame=function(){
-    word1 = new word.wordKey(wordToUse);
-    word1.returnChar();
-    word1.toString();
+
+
+var startGame = function () {
+    guessLeft=0;
+    inquirer.prompt([{
+        type: "confirm",
+        name: "startGame",
+        message: "\n \n**************************\nWelcome to the Node Word guess game." + "\n" + "You have 10 guesses to guess a word right! Do you want to continue?\n**************************\n"
+    }]).then(function (answer) {
+        if (answer.startGame) {
+            console.log("Let's begin!");
+            console.log(wordToUse);
+            word1 = new word.wordKey(wordToUse);
+            word1.returnChar();
+            word1.toString();
+            letterObj=word1.letterArr;
+            inquire();
+        } else {
+            console.log("\nSad to see you go! Next time then!\n");
+        }
+    });
 };
 startGame();
 
+var guessLeft = 0;
+var letterObj;
+var allGuessed=[];
 
+/*---------------------picking a letter after game starts------------------------ */
 var checkinput = function (answer) {
-    console.log("you picked " + answer.letter);
+    console.log("\nyou picked " + answer.letter);
+    console.log("You have "+(10-guessLeft)+" guesses remaining\n")
     word1.guessString(answer.letter);
     word1.toString();
     inquire();
 }
-
-inquire();
 function inquire() {
+    console.log(letterObj.length);
+    allGuessed=[];
+    for(ele in letterObj){
+        if(letterObj[ele].guessed){
+        allGuessed.push(true);
+        }
+    }
+
+    if(allGuessed.length==letterObj.length && guessLeft<=10){
+        console.log("\n***********--YES! YOU WON!--***********\n");
+        restartGame();
+        return;
+    }
+
     inquirer.prompt([{
         type: "input",
         name: "letter",
         message: "Pick a letter"
-    }]).then(checkinput)
+    }]).then(function (answer) {
+        guessLeft++;
+        if (guessLeft == 10) {
+            console.log("Game Over!");
+            restartGame();
+            return;
+        }
+        checkinput(answer);
+    });
 }
 
+
+/*-----------------------------------restart game function----------------------------------*/
+function restartGame() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "restartGame",
+        message: "\n\n**************************\nPlay Again?\n**************************\n\n"
+    }]).then(function (answer) {
+        if (answer.restartGame) {
+            startGame();
+        } else {
+            console.log("\nSad to see you go! Next time then!\n");
+        }
+    });
+}
--- word.js
+++ word.js
@@ -9,25 +9,19 @@ function word(currentWord) {
     this.returnChar = function () {
         for (var i = 0; i < this.wordArr.length; i++) {
            var temp=new letter.letterKey(this.wordArr[i]);
-           this.letterArr.push(temp);
+           this.letterArr.push(temp); 
         }
     }
     this.toString=function(){
-        
         console.log(this.letterArr.join(' '));
     }
     this.guessString = function (guess) {
         for(var i=0;i<this.letterArr.length;i++){
-            // console.log(this.letterArr[i]);
             this.letterArr[i].checkGuess(guess);
         } 
     }
+    
 };
-
-// var word1 = new word("Percy");
-// word1.returnChar();
-// word1.guessString();
-
 
 module.exports = {
     "wordKey": word
